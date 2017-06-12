e806c609d7bcb43196d2a39736690b83e6958bf3
John Lamis <lamisjohn@gmail.com>
2017-06-12 19:11:23 +0300

    Initial commit

---
+++ app/src/main/java/com/john/stockmarketsimulator/Market.java
@@ -0,0 +1,16 @@
+package com.john.stockmarketsimulator;
+
+import java.util.List;
+
+/**
+ * Created by giann on 12-Jun-17.
+ */
+
+public class Market {
+
+    protected List<Stock> stocks;
+
+    public Market(){
+        //TODO have a file as input which contains the initial data about the stocks
+    }
+}
