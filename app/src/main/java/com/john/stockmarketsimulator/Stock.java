309812768df26c93f136577d810e7d95384542dc
John Lamis <lamisjohn@gmail.com>
2017-06-12 19:11:03 +0300

    Added the constructors

--- app/src/main/java/com/john/stockmarketsimulator/Stock.java
+++ app/src/main/java/com/john/stockmarketsimulator/Stock.java
@@ -1,5 +1,7 @@
 package com.john.stockmarketsimulator;
 
+import java.util.Random;
+
 /**
  * Created by giann on 12-Jun-17.
  */
@@ -7,6 +9,17 @@ package com.john.stockmarketsimulator;
 public class Stock {
     protected String name;
     protected float price;
+
+    public Stock(String name, float price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public Stock(String name){
+        Random r = new Random();
+        this.name = name;
+        this.price = r.nextFloat() * (r.nextInt(300) + 1);
+    }
 
     public void setPrice(float price){
         this.price = price;
